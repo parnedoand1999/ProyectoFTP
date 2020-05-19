ef1271b685ddb89e801ca295fd05d631438f2bdd
parnedoand1999 <parnedoand@educacion.navarra.es>
2020-05-19 13:11:08 +0200

    Actualizacion del proyecto

---
+++ proyectoFinalCurso/includes/pie.php
@@ -0,0 +1,29 @@
+<div class="jumbotron text-center bg-secondary " style="margin-bottom:0">
+            <p>
+                ¡Gracias por su visita! Si desea Saber más sobre mosotros, clicke en este link: <a href="#">Sobre
+                    nosotros</a>
+            </p>
+
+            <div>
+                <span>Licencias: </span>
+
+
+                <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img
+                        alt="Licencia de Creative Commons" style="border-width:0"
+                        src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+
+                    <a href="http://www.w3.org/WAI/WCAG1AAA-Conformance" title="Explicación del Nivel Triple-A de 
+                            Conformidad">
+                        <img height="32" width="88" src="http://www.w3.org/WAI/wcag1AAA" alt="Icono de conformidad con el Nivel Triple-A, 
+                            de las Directrices de Accesibilidad para el 
+                            Contenido Web 1.0 del W3C-WAI"></a>
+            </div>
+
+        </div>
+
+
+
+</body>
+
+
+</html>
